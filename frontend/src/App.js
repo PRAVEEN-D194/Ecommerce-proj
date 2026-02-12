8b14a28af8fe7fcf7f21a9cf23202e5a6d4c603d
PRAVEEN-D194 <praveend12321@gmail.com>
2026-02-12 17:11:54 +0530

    cart item

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import ProductDetail from './pages/ProductDetail';
 import { useState } from 'react';
 import {ToastContainer} from 'react-toastify';
+import Cart from './components/Cart';
 function App() {
   const [cartItems, setCartItems] = useState([]);
   return (
@@ -19,6 +20,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/search" element={<Home />} />
             <Route path="/product/:id" element={<ProductDetail cartItems={cartItems} setCartItems={setCartItems}/>} />
+            <Route path="/cart" element={<Cart cartItems={cartItems} setCartItems={setCartItems}/>} />
           </Routes>
           </div>
         </Router>
